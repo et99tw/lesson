d192726a3f8cee7651788091a6abcf8ace22a83e
et99tw <et99tw@gmail.com>
2021-04-10 20:28:12 +0800

    TEST php

--- xls.php
+++ xls.php
@@ -1,13 +1,9 @@
 <?php
-    
       $filename ="test.xls";
- 
       $contents = "編號 \t 姓名 \t 職務 \t \n";
       $contents = $contents." 1 \t 小明 \t 董事長 \t \n";
       $contents = $contents." 2 \t 小華 \t 總經理 \t \n";
-      
       header('Content-type: application/ms-excel');
       header('Content-Disposition: attachment; filename='.$filename);
       echo $contents;
-    
 ?>
